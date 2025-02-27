cab1708a0b25680c7695f79fde036ab297655663
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2025-02-26 22:38:50 -0500

    added search fn

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,12 @@ use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let content = fs::read_to_string(&config.filepath)?;
-    Ok(println!("{}", content))
+    search(&config.query, &content).iter().for_each(|&ln| println!("{}", ln));
+    Ok(())
+}
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    content.lines().filter(|&s| s.contains(query)).collect()
 }
 
 pub struct Config {
@@ -16,5 +21,17 @@ impl Config {
         let query = args.get(0).ok_or("missing query parameter.")?.clone();
         let filepath = args.get(1).ok_or("missing filepath parameter.")?.clone();
         Ok(Config { query, filepath })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn single_result_search() {
+        let query = "duct";
+        let content = "Rust:\nsafe, fast, productive.\nPick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, content))
     }
 }
